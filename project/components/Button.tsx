b64d5b02a6f19dd69702c2d167cc3b2fbc657500
Kirill Salfetnik <oriole001@gmail.com>
2023-08-24 19:08:11 +0200

    Finish the basic layout

--- project/components/Button.tsx
+++ project/components/Button.tsx
@@ -17,7 +17,7 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
             className={twMerge(`
                 w-full
                 rounded-full
-                bg-green-500
+                bg-red-400
                 border
                 border-transparent
                 px-3
