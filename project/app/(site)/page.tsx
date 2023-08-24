c1f71618103815773bbdb02c2a675f53f503d9ae
Kirill Salfetnik <oriole001@gmail.com>
2023-08-24 17:53:24 +0200

    Create a Button component

--- project/app/(site)/page.tsx
+++ project/app/(site)/page.tsx
@@ -1,7 +1,18 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <div className="text-orange-500">
-      Main Content
+    <div className="
+      bg-neutral-900
+      rounded-lg
+      h-full
+      w-full
+      overflow-hidden
+      overflow-y-auto
+    ">
+      <Header>
+        
+      </Header>
     </div>
   )
 }
